c6fc7d914d68384f9356b0da501daccc15e836af
Rhuan Thales <rhuantst@gmail.com>
2020-09-25 13:20:17 -0300

    Iniciando a camada generica

---
+++ Shop.Mtg/Shop.Mtg.Dados.Entity/TypeConfiguration/CartaTypeConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Shop.Mtg.Dominio;
+using Shop.Mtg.Generica.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Mtg.Dados.Entity.TypeConfiguration
+{
+    class CartaTypeConfiguration : ShopEntityAbstractConfig<Carta>
+    {
+        protected override void configurarNomeTabela()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void configurarChavePrimaria()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void configurarCamposTabela()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void configurarChaveEstrangeira()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Shop.Mtg/Shop.Mtg.Generica.Entity/ShopEntityAbstractConfig.cs
+++ Shop.Mtg/Shop.Mtg.Generica.Entity/ShopEntityAbstractConfig.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shop.Mtg.Generica.Entity
 {
-    public class ShopEntityAbstractConfig
+    public abstract class ShopEntityAbstractConfig<TEntidade> : EntityTypeConfiguration<TEntidade>
+        where TEntidade : class
     {
+        public ShopEntityAbstractConfig()
+        {
+            configurarNomeTabela();
+            configurarCamposTabela();
+            configurarChavePrimaria();
+            configurarChaveEstrangeira();
+        }
 
+        protected abstract void configurarNomeTabela();
+        protected abstract void configurarCamposTabela();
+        protected abstract void configurarChavePrimaria();
+        protected abstract void configurarChaveEstrangeira();
     }
 }
